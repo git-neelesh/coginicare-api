2214d7518422b8978fb1b7662fbcdf45add72273
git-neelesh <uno.neelesh@gmail.com>
2024-07-04 21:48:09 +0530

    Logger added

--- src/main/java/com/cognicare/api/gateway/controllers/LocationController.java
+++ src/main/java/com/cognicare/api/gateway/controllers/LocationController.java
@@ -6,23 +6,28 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 @CrossOrigin
 @RestController
 @RequestMapping("/locations")
 public class LocationController {
 
+  Logger logger = Logger.getLogger("LocationController.class");
+
   @Autowired
   LocationService locationService;
 
   @PostMapping
   public Location createLocation(@RequestBody Location location) {
+    logger.info("Create location called.");
     locationService.createLocation(location);
     return location;
   }
 
   @GetMapping
   public List<Location> getLocations() {
+    logger.info("Fetching locations called.");
     return locationService.getLocations();
   }
 }
